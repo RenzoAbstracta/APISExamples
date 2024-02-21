1581c493fc94a8a209ee89beff386a63d0a9e8bf
Laura Gayo <laura.gayo@abstracta.com.uy>
2024-02-21 15:20:58 -0300

    Changed response to string and added transformers property to the mapping

--- bank_module/index.js
+++ bank_module/index.js
@@ -11,7 +11,7 @@ app.post('/payment', (req, res) => {
     Math.floor(Math.random() * (999999 - 1000)) + 1000
   ).toString();
   payment.amount = req.body.amount.toString();
-  payment.account = req.body.account;
+  payment.account = req.body.account.toString();
 
   var strPayment = JSON.stringify(payment);
   res.send(JSON.parse(strPayment));
